d57f44b2d5fac86403c74a392061f4b57a2585ea
cloudyan <1395093509@qq.com>
2020-05-27 21:36:35 +0800

    feat: update docs

---
+++ docs/function/function.polyfill.js
@@ -0,0 +1,51 @@
+// 实现一个call函数
+// 将要改变this指向的方法挂到目标this上执行并返回
+Function.prototype.mycall = function (context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('not funciton')
+  }
+  context = context || window
+  context.fn = this
+  let arg = [...arguments].slice(1)
+  let result = context.fn(...arg)
+  delete context.fn
+  return result
+}
+
+// 实现一个apply函数
+Function.prototype.myapply = function (context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('not funciton')
+  }
+  context = context || window
+  context.fn = this
+  let result
+  if (arguments[1]) {
+    result = context.fn(...arguments[1])
+  } else {
+    result = context.fn()
+  }
+  delete context.fn
+  return result
+}
+
+// 实现一个bind函数
+Function.prototype.mybind = function (context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Error')
+  }
+  let _this = this
+  let arg = [...arguments].slice(1)
+  return function F() {
+    // 处理函数使用new的情况
+    if (this instanceof F) {
+      return new _this(...arg, ...arguments)
+    } else {
+      return _this.apply(context, arg.concat(...arguments))
+    }
+  }
+}
+
+参考：
+
+- https://mp.weixin.qq.com/s/620g6Gw6KF1N-jWUguw8yQ
