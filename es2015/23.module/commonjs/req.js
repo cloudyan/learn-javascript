4259256ca15d29a2719e545a1bb458846f1ee651
cloudyan <1395093509@qq.com>
2020-09-09 19:06:23 +0800

    feat: add require

---
+++ es2015/23.module/commonjs/req.js
@@ -0,0 +1,8 @@
+
+// Node.js 中的 require 是如何工作的？
+// https://mp.weixin.qq.com/s/usS7VUXiG7urs573pPO9bQ
+(function (exports, require, module, __filename, __dirname) {
+  module.exports = function () {
+    console.log(__dirname);
+  }
+})
