b0ab97cad46a9cd72ff859c2fe0a1cb4b9fe0d9e
yanzhanjun <yanzhanjun@lattebank.com>
2020-12-30 13:17:45 +0800

    feat: update module loop

---
+++ es2015/23.module/loop/es6-1/index.mjs
@@ -0,0 +1,5 @@
+
+// test
+
+// node --experimental-modules a.mjs
+// nodejs@13.2.0 之前想要在node中使用ES modules，需要添加`--experimental-module`
--- es2015/23.module/loop/es6/a.mjs
+++ es2015/23.module/loop/es6/a.mjs
@@ -1,8 +1,13 @@
 // a.mjs
-console.log('a is running')
-import { b } from './b.mjs';
-console.log('in a, b =');
-console.log(b);
-export let a = 'a';
-setTimeout(() => console.log('bar = %j after 500 ms', b), 500);
-console.log('a is finished');
+console.log('\na starting')
+var obj = {
+  done: false,
+  set(v) {
+    this.done = v
+  },
+};
+import b from './b.mjs';
+console.log('in a, b.done =', b.done);
+obj.set(true)
+export default obj;
+console.log('a done\n');
