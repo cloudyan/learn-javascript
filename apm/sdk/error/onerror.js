7692e8124e70bea45126ad37d58c5319b84034a6
cloudyan <1395093509@qq.com>
2022-03-09 15:46:54 +0800

    feat: update script error example

--- apm/sdk/error/onerror.js
+++ apm/sdk/error/onerror.js
@@ -42,7 +42,7 @@ export function uncaughtOnError(callback = noop) {
     if (lineno === 0 && regCrosError.test(event)) {
       // Script error: See Browser Console for Detail
       // 此错误也需要上报，我需要知道那个链接的资源出了这个问题，同一个资源只需控制上报一次即可
-      console.warn('Script Error: See Browser Console for Detail. See https://tinyurl.com/yztq2q5o');
+      console.warn('Script error. -> See Browser Console for Detail. See https://tinyurl.com/yztq2q5o');
       // console.warn('Ignoring cross-domain or eval script error. See https://tinyurl.com/yztq2q5o');
       // https://segmentfault.com/a/1190000020756584
     } else {
